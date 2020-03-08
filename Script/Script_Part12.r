f22260bb976fd4aa56ee98a2963312ca6b0a21a0
JayoungKim82 <jayoung82.kim@gmail.com>
2020-03-08 12:41:27 +0900

    mpg 파일 로드하기 mpg 파일을 로드하였습니다.

--- Script/Script_Part12.r
+++ Script/Script_Part12.r
@@ -5,6 +5,7 @@ install.packages("plotly")
 library(plotly)
 
 library(ggplot2)
+mpg <- as.data.frame(ggplot2::mpg)
 p <- ggplot(data = mpg, aes(x = displ, y = hwy, col = drv)) + geom_point()
 
 ggplotly(p)
